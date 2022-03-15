7da3c240ceaec407af27acfb041a3f46b83b8d2d
zuhaib <mcf2000656@ue.edu.pk>
2022-03-15 20:20:12 +0500

    init

--- database/migrations/2022_03_15_141808_create_categories_table.php
+++ database/migrations/2022_03_15_141808_create_categories_table.php
@@ -13,16 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image');
-            $table->string('phone');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
--- database/migrations/2022_03_15_150723_create_ads_table.php
+++ database/migrations/2022_03_15_150723_create_ads_table.php
@@ -15,6 +15,19 @@ return new class extends Migration
     {
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('condition')->default(False);
+            $table->string('location');
+            $table->string('specifications');
+            $table->integer('price');
+            $table->string('images');
+            $table->boolean('is_negotiable')->default(False);
+            $table->boolean('condition');
+            $table->unsignedBigInteger('seller_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('seller_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
