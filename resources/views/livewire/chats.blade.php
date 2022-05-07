7b89d71975b9959759d80ffdccbb442ee31c4815
zuhaib <zuhaibranaa@gmail.com>
2022-05-08 01:21:43 +0500

    chat Moduke FIxed

--- resources/views/livewire/chats.blade.php
+++ resources/views/livewire/chats.blade.php
@@ -323,12 +323,17 @@
                         </div>
                         <div class="chat-message clearfix">
                             <div class="input-group mb-0">
-                                <div class="input-group-prepend">
+                                <div onclick="document.getElementById('newMessageForm').submit()" class="input-group-prepend">
                                     <span class="input-group-text"><i class="fa fa-send"></i></span>
                                 </div>
-                                <input type="text" class="form-control" placeholder="Enter text here...">
+                                <input form="newMessageForm" name="messge" type="text" class="form-control" placeholder="Enter text here...">
                             </div>
                         </div>
+                        <form action="{{ url('chat') }}" method="post" id="newMessageForm">
+                            @csrf
+                            <input type="hidden" name="from_user" value="{{ auth()->user()->id }}">
+                            <input type="hidden" name="to_user" value="{{ $_COOKIE['val'] }}">
+                        </form>
                     </div>
                 </div>
             </div>
