9d310ca41997e05aeb75f02d93b6148d7da605c3
zuhaib <zuhaibranaa@gmail.com>
2022-05-08 23:12:00 +0500

    ad module completed completely

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -112,13 +112,15 @@ class AdController extends Controller
      */
     public function update(Request $request,Ad $ad)
     {
-        $ad->title = $request->title;
+        if (auth()->user()->is_admin && $ad->seller_id == auth()->user()->id) {
+            $ad->status = 1;
+        }else{
+            $ad->title = $request->title;
         $ad->description = $request->description;
         $ad->brand = $request->brand;
         $ad->model = $request->model;
         $ad->health = $request->health;
         $ad->condition = $request->condition;
-        $ad->status = 0;
         $ad->location = $request->location;
         $ad->specifications = $request->specifications;
         $ad->price = $request->price;
@@ -137,8 +139,7 @@ class AdController extends Controller
         }else{
             $ad->is_negotiable = $request->is_negotiable;
         }
-        $ad->seller_id = auth()->user()->id;
-        $ad->category_id = $request->category_id;
+        }
         $ad->save();
         return redirect('dashboard');
     }
